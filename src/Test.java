82876d810239cb6d25c14ef1cb3594b581e9df39
weihuaxiao <327658337@qq.com>
2017-03-12 19:22:55 +0800

    ceshi

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.print(MD5.getMD5("a89171fcf9ebaaa0".getBytes()));
+	}
+
+}
